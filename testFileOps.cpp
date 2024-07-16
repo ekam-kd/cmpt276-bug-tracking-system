9221b9d61c9836040515ec10e7a6363b6eb5a3cd
CapriShorts <ashleykim156@gmail.com>
2024-07-15 22:42:44 -0700

    new unit test mostly done

--- testFileOps.cpp
+++ testFileOps.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   testFileOps.cpp
  * Author: Ashley Kim
  * Created: July 2024
@@ -17,69 +17,94 @@
 #include "definitions.hpp"
 using namespace std;
 
-void main_test_ops(){
-// constructor
-        ChangeItem new_item = ChangeItem();
-        // // destructor
-        // ~ChangeItem();
+void main_test_ops()
+{
+
+    // public methods of product
+    //  constructor
+    Customer new_customer = Customer();
+
+    // set_name is dependent on get_name to pass.
+    cout << "Testing set_name() and get_name():" << endl;
+    new_customer.set_name("John Doe");
+    if (new_customer.get_name() == "John Doe")
+    {
+        cout << "set_name() test PASSED" << endl;
+        cout << "get_name() test PASSED" << endl;
+    }
+    else
+    {
+        cout << "set_name() test FAILED" << endl;
+        cout << "get_name() test FAILED" << endl;
+    }
+
+    // set_email is dependent on get_email to pass.
+    cout << "Testing set_email() and get_email():" << endl;
+    new_customer.set_email("JohnDoe@company.ca");
+    if (new_customer.get_email() == "JohnDoe@company.ca")
+    {
+        cout << "set_email() test PASSED" << endl;
+        cout << "get_email() test PASSED" << endl;
+    }
+    else
+    {
+        cout << "set_email() test FAILED" << endl;
+        cout << "get_email() test FAILED" << endl;
+    }
+
+    // set_phone is dependent on get_phone to pass.
+    cout << "Testing set_phone() and get_phone():" << endl;
+    new_customer.set_phone("12345678910");
+    if (new_customer.get_phone() == "12345678910")
+    {
+        cout << "set_phone() test PASSED" << endl;
+        cout << "set_phone() test PASSED" << endl;
+    }
+    else
+    {
+        cout << "set_phone() test FAILED" << endl;
+        cout << "get_phone() test FAILED" << endl;
+    }
+
+    // set_department is dependent on get_department to pass.
+    cout << "Testing set_department() and get_department():" << endl;
+    new_customer.set_department("sales");
+    if (new_customer.get_department() == "sales")
+    {
+        cout << "set_department() test PASSED" << endl;
+        cout << "set_department() test PASSED" << endl;
+    }
+    else
+    {
+        cout << "set_department() test FAILED" << endl;
+        cout << "get_department() test FAILED" << endl;
+    }    
 
 
-        // testing set_id()
-        cout << "testing set_id() -- set to '12345" << endl;
-        if(new_item.set_id(12345)){
-            cout << "Set id test passed." << endl;
-        }
-        // testing set_productName() to acrobat
-        cout << "testing set_productName() -- set to Acrobat" << endl;
-        if(new_item.set_productName('Acrobat')){
-            cout << "Set product name passed." << endl;
-        }
-        // Testing set product release ID -- to rel1
-        if(new_item.set_productReleaseID('rel1')){
-            
-        }
-        // set description
-        void set_description(char* description);
-        // set status
-        void set_status(char* status);
-        // set priority
-        void set_priority(int priority);
-        // set requests
-        void set_requests(int requests);
+    // // print customer info
+    // void print_customer_info();
 
 
 
+    // // register new customer
+    // void register_customer();
 
-        // get id
-        long int get_id();
-        // get product name
-        char* get_productName();
-        // get product release ID
-        char* get_productReleaseID();
-        // get description
-        char* get_description();
-        // get status
-        char* get_status();
-        // get priority
-        int get_priority();
-        // get requests
-        int get_requests();
-        // set id
-        void set_id(long int id);
-        // set product name
-        void set_productName(char* productName);
-        // set product release ID
-        void set_productReleaseID(char* productReleaseID);
-        // set description
-        void set_description(char* description);
-        // set status
-        void set_status(char* status);
-        // set priority
-        void set_priority(int priority);
-        // set requests
-        void set_requests(int requests);
-        // print change item info
-        void print_change_item_info();
+    // // helper functions
 
+    // // initialize customer database
+    // bool init_customer();
 
+    // // check if customer exists in database file
+    // bool check_customer(char *name // name of the customer
+    // );
+
+    // // add customer to file
+    // bool create_customer(Customer * customer // customer to initialize
+    // );
+    // // check of employee
+    // bool check_employee(char *name // name of the employee
+    // );
+
+    // // closes customer database file
+    // bool close_customer();
 }
