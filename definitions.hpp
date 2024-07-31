c7e45e9faf913ac54e22de3fcb2c1753593d20bf
ekam-kd <ekam.13dhanoa@gmail.com>
2024-07-31 03:13:44 -0700

    changed .dat to .bin, didnt rlly make a difference but oh well

--- definitions.hpp
+++ definitions.hpp
@@ -20,11 +20,11 @@
 #define MAX_DEPARTMENT 30
 
 // database file names
-#define CUSTOMER_FILE "customers.dat"
-#define RELEASE_FILE "releases.dat"
-#define CHANGE_ITEM_FILE "changeItems.dat"
-#define CHANGE_REQUEST_FILE "changeRequests.dat"
-#define PRODUCT_FILE "products.dat"
+#define CUSTOMER_FILE "customers.bin"
+#define RELEASE_FILE "releases.bin"
+#define CHANGE_ITEM_FILE "changeItems.bin"
+#define CHANGE_REQUEST_FILE "changeRequests.bin"
+#define PRODUCT_FILE "products.bin"
 
 
 // //global filestream for customer file so it stays open for program duration
